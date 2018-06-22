03d5fc06201d25d728273490a86edd110877e96f
hoppenic <hoppenic@gmail.com>
2018-06-22 12:19:36 -0500

    updates

--- BeerStore/Controllers/ProductController.cs
+++ BeerStore/Controllers/ProductController.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BeerStore.Models;
 
 namespace BeerStore.Controllers
 {
     public class ProductController : Controller
     {
+
+        //this is my list called _products, using the Product Class
+        private List<Product> _products;
+
+        public ProductController()
+        {
+            //for now use list to mock up data
+
+
+        }
+
+
+
         public IActionResult Index()
         {
             return View();
