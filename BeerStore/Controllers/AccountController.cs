c93a9816c450078b6510a46c33c1c6573f5b66e6
hoppenic <hoppenic@gmail.com>
2018-06-22 13:28:19 -0500

    update

---
+++ BeerStore/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BeerStore.Models;
+
+namespace BeerStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //responds on GET /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        //responds on POST /Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(string username, string password)
+        {
+            return Ok();
+        }
+
+
+
+    }
+}
---
+++ BeerStore/Models/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using BeerStore.Models;
+
+namespace BeerStore.Models
+{
+    public class RegisterViewModel
+    {
+
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string  PhoneNumber { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+
+
+
+    }
+}
