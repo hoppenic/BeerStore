e4091c1fd615781c1e7ff2fc2442d867d6147385
hoppenic <hoppenic@gmail.com>
2018-06-26 15:27:52 -0500

    update

---
+++ BeerStore/Controllers/ProductsAdminController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BeerStore.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace BeerStore.Controllers
+{
+    public class ProductsAdminController : Controller
+    {
+        private readonly BeerStoreDbContext _context;
+        private readonly IHostingEnvironment _env;
+
+        public ProductsAdminController(BeerStoreDbContext context, IHostingEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        // GET: ProductsAdmin
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        // GET: ProductsAdmin/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: ProductsAdmin/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductsAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Description,Price,Image")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: ProductsAdmin/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.SingleOrDefaultAsync(m => m.ID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: ProductsAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product, Microsoft.AspNetCore.Http.IFormFile imageFile)
+        {
+            if (id != product.ID)
+            {
+                return NotFound();
+            }
+
+
+            string newFile = System.IO.Path.Combine(_env.WebRootPath,
+               "images", imageFile.FileName);
+
+            System.IO.FileInfo newFileInfo = new System.IO.FileInfo(newFile);
+            using (System.IO.FileStream fs = newFileInfo.Create())
+            {
+                imageFile.CopyTo(fs);
+                fs.Close();
+            }
+
+
+
+
+            if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        _context.Update(product);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!ProductExists(product.ID))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+            return View(product);
+        }
+
+        // GET: ProductsAdmin/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: ProductsAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.ID == id);
+        }
+    }
+}
